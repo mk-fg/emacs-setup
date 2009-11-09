fd4152fc03b57514a61c8a3c5e0614f77c1d5544
fraggod@malediction <mk.fraggod@gmail.com>
2009-11-09 10:58:18 +0500

    emms-related fixes/updates

--- core/fg_emms.el
+++ core/fg_emms.el
@@ -1,11 +1,13 @@
-(require 'emms-player-simple)
 (require 'emms-source-file)
 (require 'emms-source-playlist)
+(require 'emms-player-mplayer)
+(require 'emms-playlist-mode)
 (require 'emms-info-libtag)
 
 (add-hook 'emms-player-started-hook 'emms-show)
 
 (setq
-	emms-player-list '(emms-player-mplayer)
+	emms-player-list
+		'(emms-player-mplayer-playlist emms-player-mplayer)
 	emms-info-functions '(emms-info-libtag)
 	emms-show-format "NP: %s")
