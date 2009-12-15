6d65b31c3002f6f33c9cc8679c4c252a268a6720
fraggod@coercion <mk.fraggod@gmail.com>
2009-12-15 12:00:07 +0500

    lisp: commented out slime-dependent parts

--- core/fg_lisp.el
+++ core/fg_lisp.el
@@ -1,10 +1,10 @@
 (require 'cl) ; superb stuff
-(setq inferior-lisp-program "/usr/bin/sbcl")
 
-(require 'slime)
-(slime-setup)
+;(setq inferior-lisp-program "/usr/bin/sbcl")
+;(require 'slime)
+;(slime-setup)
+;(add-to-list 'auto-mode-alist
+;	'("\\.cl$" . lisp-mode))
 
-(add-to-list 'auto-mode-alist
-	'("\\.cl$" . lisp-mode))
 (add-to-list 'auto-mode-alist
 	'("/\\.?emacs\\(\\.d/.*\\.el\\)?$" . lisp-interaction-mode))
