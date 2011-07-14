ce678592b20045dd07ad4cafdb7877be1aac2f87
Mike Kazantsev <mk.fraggod@gmail.com>
2011-07-14 12:27:06 +0600

    +hooks - very crude thing to auto-compile and push files on save

---
+++ hooks.el
@@ -0,0 +1,11 @@
+(defun fg-hook-autopush ()
+	(message (file-relative-name (buffer-file-name)
+		"/home/fraggod/hatch/e1/public_transport/local_trains/"))
+	(when
+		(member
+			(file-relative-name (buffer-file-name)
+				"/home/fraggod/hatch/e1/public_transport/local_trains/")
+			'("embed/style.scss" "embed/route_details.coffee" "route_web.php"))
+		(start-process "fg-push" "fg-push" "/home/fraggod/bin/fgpush")))
+
+(add-hook 'after-save-hook 'fg-hook-autopush)
