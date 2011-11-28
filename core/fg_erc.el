d9d8a649b41ed5f01605991de912ee9e884a4ce1
Mike Kazantsev <mk.fraggod@gmail.com>
2011-11-29 04:18:00 +0600

    erc: -quakenet

--- core/fg_erc.el
+++ core/fg_erc.el
@@ -9,8 +9,6 @@
 		:password fg-auth-erc-password-freenode :full-name "Mike Kazantsev")
 	(erc-tls :server "irc.fraggod.net" :port 6667 :nick "oftc"
 		:password fg-auth-erc-password-oftc :full-name "Mike Kazantsev")
-	(erc-tls :server "irc.fraggod.net" :port 6667 :nick "quakenet"
-		:password fg-auth-erc-password-quakenet :full-name "Mike Kazantsev")
 	(erc-tls :server "irc.fraggod.net" :port 6667 :nick "bitlbee"
 		:password fg-auth-erc-password-bitlbee :full-name "Mike Kazantsev"))
 
