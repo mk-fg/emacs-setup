6255709d98f6953db724bfdf48109d22c750a1b5
Mike Kazantsev <mk.fraggod@gmail.com>
2013-06-17 13:07:37 +0600

    erc: fix for cjdns/hype i2p bridge-bot ignore-regexp

--- core/fg_erc.el
+++ core/fg_erc.el
@@ -122,7 +122,7 @@ specified nickname, including ZNC-buffered messages."
 				("unposted"
 					,(concat "\\[\\(" "website\\(/master\\)?"
 						"\\|remoteStorage\\.js\\(/[[:word:]\-_]+\\)?" "\\)\\]\\s-+"))
-				("i2p" "(<--|-->)\s-+\\S-+ has (joined|quit) ")
+				("i2p" "\\(<--\\|-->\\)\\s-+\\S-+ has \\(joined\\|quit\\) ")
 				("fc[a-f0-9]+" "\\S-+ is over two months out of date. ya feeling ok\\?")
 				("DeBot" "\\[\\(URL\\|feed\\)\\]\\s-+")))
 
