86c27472756aece5ee1d9e7bb8dabb23d57433d1
Mike Kazantsev <mk.fraggod@gmail.com>
2013-07-03 14:23:42 +0600

    macros: fixed weird leftover bug with indent-tabs-mode forcing in fg-tab func, leftover from 402f348

--- core/fg_macroz.el
+++ core/fg_macroz.el
@@ -481,7 +481,6 @@ In all other buffers: if ARG is \\[universal-argument], calls
 `smart-indent'. Else if point is at the end of a symbol,
 expands it. Else calls `smart-indent'."
 	(interactive "p")
-	(setq indent-tabs-mode t)
 	(labels
 		((fg-tab-must-expand (&optional arg)
 			(unless
