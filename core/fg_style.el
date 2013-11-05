f7a837eef36764e2ce92d8485ff9799e458333c7
Mike Kazantsev <mk.fraggod@gmail.com>
2013-11-05 12:35:58 +0600

    style: disable lua "electric" braces

--- core/fg_style.el
+++ core/fg_style.el
@@ -5,14 +5,17 @@
 	default-tab-width 2
 	indent-region-function
 		'fg-indent-command ; default one inserts spaces
-	py-indent-offset 2
-	python-indent 2
-	python-guess-indent nil ; this disables spaces/tab flip in py buffers
 	basic-indent 2
 	require-final-newline t
 	tab-always-indent t ; overidden by fg-tab
+	fill-column 80
+
+	py-indent-offset 2
+	python-indent 2
+	python-guess-indent nil ; this disables spaces/tab flip in py buffers
+
 	c-syntactic-indentation nil ; stupid ten-tabs indentation
-	fill-column 80)
+	lua-electric-flag nil) ;; disable indents on closing brackets
 
 ;; Make absolutely sure python-mode uses tabs
 (add-hook 'python-mode-hook
