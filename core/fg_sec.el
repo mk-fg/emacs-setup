33634b2036e648055cd4b0cf75e3466e620afcb3
Mike Kazantsev <mk.fraggod@gmail.com>
2014-12-07 17:01:24 +0500

    sec: syntax fix

--- core/fg_sec.el
+++ core/fg_sec.el
@@ -73,50 +73,50 @@ Same as `epa-file-select-keys', but always picks key matching `epa-select-keys-d
 (when
 	(or (> emacs-major-version 24)
 		(and (= emacs-major-version 24) (> emacs-minor-version 4)))
-(defun epg--list-keys-1 (context name mode)
-	(let
-		((args
-				(append
-					(if epg-gpg-home-directory
-						(list "--homedir" epg-gpg-home-directory))
-					'("--with-colons" "--no-greeting" "--batch"
-						"--with-fingerprint" "--with-fingerprint")
-					(unless
-						(eq (epg-context-protocol context) 'CMS)
-						'("--fixed-list-mode"))))
-			(list-keys-option
-				(if (memq mode '(t secret))
-					"--list-secret-keys"
-					(if (memq mode '(nil public)) "--list-keys" "--list-sigs")))
-			(coding-system-for-read 'binary)
-			keys string field index)
-		(if name
-			(progn
-				(unless (listp name) (setq name (list name)))
-				(while name
-					(setq
-						args (append args (list list-keys-option (car name)))
-						name (cdr name))))
-			(setq args (append args (list list-keys-option))))
-			(with-temp-buffer
-				(apply #'call-process
-					(if (eq (epg-context-protocol context) 'CMS)
-						epg-gpgsm-program epg-gpg-program)
-					nil (list t nil) nil args)
-				(goto-char (point-min))
-				(while (re-search-forward "^[a-z][a-z][a-z]:.*" nil t)
-					(setq
-						keys (cons (make-vector 15 nil) keys)
-						string (match-string 0)
-						index 0
-						field 0)
-					(while
-						(and (< field (length (car keys)))
-							(eq index (string-match "\\([^:]+\\)?:" string index)))
-						(setq index (match-end 0))
-						(aset (car keys) field (match-string 1 string))
-						(setq field (1+ field))))
-				(nreverse keys))))
+	(defun epg--list-keys-1 (context name mode)
+		(let
+			((args
+					(append
+						(if epg-gpg-home-directory
+							(list "--homedir" epg-gpg-home-directory))
+						'("--with-colons" "--no-greeting" "--batch"
+							"--with-fingerprint" "--with-fingerprint")
+						(unless
+							(eq (epg-context-protocol context) 'CMS)
+							'("--fixed-list-mode"))))
+				(list-keys-option
+					(if (memq mode '(t secret))
+						"--list-secret-keys"
+						(if (memq mode '(nil public)) "--list-keys" "--list-sigs")))
+				(coding-system-for-read 'binary)
+				keys string field index)
+			(if name
+				(progn
+					(unless (listp name) (setq name (list name)))
+					(while name
+						(setq
+							args (append args (list list-keys-option (car name)))
+							name (cdr name))))
+				(setq args (append args (list list-keys-option))))
+				(with-temp-buffer
+					(apply #'call-process
+						(if (eq (epg-context-protocol context) 'CMS)
+							epg-gpgsm-program epg-gpg-program)
+						nil (list t nil) nil args)
+					(goto-char (point-min))
+					(while (re-search-forward "^[a-z][a-z][a-z]:.*" nil t)
+						(setq
+							keys (cons (make-vector 15 nil) keys)
+							string (match-string 0)
+							index 0
+							field 0)
+						(while
+							(and (< field (length (car keys)))
+								(eq index (string-match "\\([^:]+\\)?:" string index)))
+							(setq index (match-end 0))
+							(aset (car keys) field (match-string 1 string))
+							(setq field (1+ field))))
+					(nreverse keys)))))
 
 
 
