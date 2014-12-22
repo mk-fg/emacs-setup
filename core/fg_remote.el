ce48f3a827acd3bffc39fff410293dac7c178e82
Mike Kazantsev <mk.fraggod@gmail.com>
2014-12-22 15:11:08 +0500

    remote: add switch-sockets, get-socket-type

--- core/fg_remote.el
+++ core/fg_remote.el
@@ -1,15 +1,49 @@
 ;; Stuff to call via emacsclient
 
+(require 'dns)
+(setq-default
+	server-use-tcp nil
+	server-host ;; ipv6 stuff doesn't seem to be supported yet
+		(dns-query-cached (concat system-name ".v4c"))
+	server-port 6002)
+
+
 (defun fg-remote (data)
 	"Writes stringified DATA to a /tmp/.ece.remote.XXXX temp-file and returns filename.
 Path and filename prefix are fixed and independent of e.g. `temporary-file-directory'.
+Final newline is ensured in DATA after converting it to string.
+If DATA is nil, file won't be created.
+
 Intended use is to call this wrapper on something that returns string
 from emacsclient, match returned filename, read file contents, remove it."
-	(let*
-		((temporary-file-directory "/tmp/")
-			(tmp (make-temp-file ".ece.remote.")))
-		(append-to-file (format "%s" data) nil tmp)
-		tmp))
+	(when data
+		(let*
+			((temporary-file-directory "/tmp/")
+				(tmp (make-temp-file ".ece.remote.")))
+			(set 'data (format "%s" data))
+			(unless (s-ends-with? "\n" data) (set 'data (concat data "\n")))
+			(append-to-file data nil tmp)
+			tmp)))
+
+
+(defun fg-remote-switch-sockets (&optional tcp-or-unix)
+	"Switch server between listening on tcp or unix socket, force-killing all clients.
+Optional argument can be 'tcp or 'unix,
+otherwise socket type is toggled based on `server-use-tcp'."
+	(interactive)
+	(set 'server-use-tcp
+		(cond
+			((eq tcp-or-unix 'tcp) t)
+			((eq tcp-or-unix 'unix) nil)
+			((not tcp-or-unix) (not server-use-tcp))
+			(t (error "Invalid value for TCP-OR-UNIX: %s" tcp-or-unix))))
+	(server-force-stop)
+	(server-start)
+	nil) ; clients will never see the result
+
+(defun fg-remote-get-socket-type ()
+	"Return current socket type in use as a 'tcp or 'unix sym."
+	(if server-use-tcp 'tcp 'unix))
 
 
 (defun fg-remote-list-buffers (&optional pattern)
