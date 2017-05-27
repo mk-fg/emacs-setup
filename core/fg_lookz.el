79543eafd95019dae9016814eb6886b984d2430f
Mike Kazantsev <mk.fraggod@gmail.com>
2017-05-28 03:18:04 +0500

    looks: switch to liberation fonts for now

--- core/fg_lookz.el
+++ core/fg_lookz.el
@@ -19,10 +19,10 @@
 ;; - use font-ranges-to-elisp.py script for easier copy-pasting from wikipedia
 ;; - https://www.gnu.org/software/emacs/manual/html_node/emacs/Fonts.html
 (when window-system
-	(set-frame-font "Luxi Sans-9")
-	(set-face-font 'variable-pitch "Luxi Sans-9")
-	(set-face-font 'fixed-pitch "DejaVu Sans Mono-9")
-	(set-fontset-font t '(?А . ?я) "DejaVu Sans-9")
+	(set-frame-font "Liberation Sans-9")
+	(set-face-font 'variable-pitch "Liberation Sans-9")
+	(set-face-font 'fixed-pitch "Liberation Mono-9")
+	(set-fontset-font t '(?А . ?я) "Liberation Sans-9")
 	;; Emoji ranges from https://en.wikipedia.org/wiki/Emoji
 	(dolist
 		(range-cons
