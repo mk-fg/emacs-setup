2f82813abcee49cff658ef2e23772491defda38b
Mike Kazantsev <mk.fraggod@gmail.com>
2024-02-24 02:59:58 +0500

    macros: revert regexp tweaks in fg-xdg-open - seem to have issues, not needed

--- core/fg_macros.el
+++ core/fg_macros.el
@@ -663,7 +663,7 @@ If BUFFERS is passed, only these will be considered."
 	(--filter
 		(and
 			(or (not pattern) (s-contains? pattern it t))
-			(not (s-matches? "\\`\\s-*\\*" it)))
+			(not (s-matches? "^\\s-*\\*" it)))
 		(-map 'buffer-name (or buffers (buffer-list)))))
 
 (defun fg-get-useful-buffer (&optional pattern buffers)
@@ -944,37 +944,37 @@ URL is stripped via `s-trim', checked to be non-empty, with message issued about
 (defun fg-xdg-open-this-url-at-point ()
 	"Similar to `thing-at-cursor' with 'word, but limits word by whitespaces only.
 Strips common junk around URLs via `fg-xdg-open-this-trim-url'.
-Auto-adds http:// schema if there is none in the URL."
+Auto-adds https:// schema if there is none in the URL."
 	(interactive)
-	(let ((url-chars "\\`[[:space:]\n") url)
+	(let ((url-chars "^[[:space:]\n") url)
 		(save-excursion
 			(skip-chars-backward url-chars)
 			(setq url (point))
 			(skip-chars-forward url-chars)
 			(setq url (buffer-substring-no-properties url (point))))
 		(setq url (fg-xdg-open-this-trim-url url))
-		(if (string-match "\\`[a-zA-Z0-9]+:" url) url (concat "http://" url))))
+		(if (string-match "^[a-zA-Z0-9]+:" url) url (concat "https://" url))))
 
 (defun fg-xdg-open-this-trim-url (url)
 	"Trims and cleans URL from any common junk around it.
 Example junk is braces/brackets, quotes, commas/periods, etc."
 	(setq url (s-trim url))
 	;; Remove trailing commas/periods first, for matching brace-stripping to work next
-	;; Example: (http://myurl.com/).
-	(when (string-match "[,.]\\'" url)
+	;; Example: (https://myurl.com/).
+	(when (string-match "[,.]$" url)
 		(let ((c (s-right 1 url)))
 			(unless (s-contains? c (s-left (1- (length url)) url))
 				(setq url (s-trim (s-chop-suffix c url))))))
-	(setq url (replace-regexp-in-string "\\(\\`[|]+\\|[|]+\\'\\)" "" url t t))
+	(setq url (replace-regexp-in-string "\\(^[|]+\\|[|]+$\\)" "" url t t))
 	;; Remove matching braces/brackets/quotes around it or just leading ones
-	;; Examples: (http://myurl.com/) "http://myurl.com/
-	(while (string-match "\\`[({\\[<\"'`]" url)
+	;; Examples: (https://myurl.com/) "https://myurl.com/
+	(while (string-match "^[({\\[<\"'`]" url)
 		(let ((c (s-left 1 url)))
 			(setq url (s-trim (s-chop-suffix c (s-chop-prefix c url))))))
 	;; Remove stuff at the end if they are not opened somewhere in the middle
 	;; Specifically: braces/brackets, quotes, periods/commas
-	;; Example: http://myurl.com/] http://myurl.com/),
-	(when (string-match "[])}>\"'`,.]\\'" url)
+	;; Example: https://myurl.com/] https://myurl.com/),
+	(when (string-match "[])}>\"'`,.]$" url)
 		(let ((c (s-right 1 url)))
 			(unless (s-contains? c (s-left (1- (length url)) url))
 				(setq url (s-trim (s-chop-suffix c url))))))
